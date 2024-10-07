21f842e1c1ebc89dadf26d1a810e20de15d45e25
Marcelo Sola Bru <marcelosola2001@gmail.com>
2024-10-07 19:31:05 -0300

    services, revisar FK producto

--- app/models/compras.py
+++ app/models/compras.py
@@ -4,6 +4,22 @@ from app import db
 class Compras(db.Model):
     __tablename__ = "compras"
     id = db.Column(db.Integer, primary_key=True)
-    producto_id = db.Column(db.Integer, db.ForeignKey("productos.id"))
+    producto_id = db.Column(db.Integer)
     fecha_compra = db.Column(db.DateTime)
     direccion_envio = db.Column(db.String(255))
+
+
+    @staticmethod
+    def crear_compra(producto_id, direccion_envio):
+        nueva_compra = Compras(producto_id=producto_id, direccion_envio=direccion_envio)
+        db.session.add(nueva_compra)
+        db.session.commit()
+        return nueva_compra
+
+    @staticmethod
+    def obtener_compras():
+        return Compras.query.all()
+
+    @staticmethod
+    def obtener_compra_por_id(compra_id):
+        return Compras.query.get(compra_id)
