34df1e7b53132d78b723f85cff497eaf4bf467bb
Francisco Cisneros <franbichir@gmail.com>
2024-12-01 16:11:06 -0300

    arreglada la config y env

--- app/config/config.py
+++ app/config/config.py
@@ -11,7 +11,6 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     @staticmethod
     def init_app(app):
@@ -22,6 +21,10 @@ class DevelopmentConfig(Config):
     TESTING = True
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_DEV_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"options": "-csearch_path=compras_schema"}
+    }
     CACHE_REDIS_URL = os.getenv("REDIS_URL")
 
 
@@ -30,6 +33,9 @@ class TestConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_TEST_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"options": "-csearch_path=compras_schema"}
+    }
     CACHE_REDIS_URL = os.getenv("REDIS_URL")
 
 
@@ -38,11 +44,10 @@ class ProductionConfig(Config):
     TESTING = False
     SQLALCHEMY_RECORD_QUERIES = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_PROD_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"options": "-csearch_path=compras_schema"}
+    }
     CACHE_REDIS_URL = os.getenv("REDIS_URL")
-
-    @classmethod
-    def init_app(cls, app):
-        Config.init_app(app)
 
 
 def factory(app):
